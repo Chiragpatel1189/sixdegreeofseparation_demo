bbc03eedc3bc0163203e810954aaf3ae085c8e4b
Chiragpatel1189 <chiragrpatel812@gmail.com>
2022-12-02 08:48:34 +0530

    Revert "Fix minor design issues."
    This reverts commit 569247a3eabe135e6ca0e1d6b6a3dae352965a81.

--- src/App.js
+++ src/App.js
@@ -5,67 +5,67 @@ import AddUser from "./components/AddUser";
 import MakeRelationship from "./components/MakeRelationship";
 
 const usersData = [
-  {
-    id: 1,
-    name: "Sameer",
-    friends: ["Aayushi", "Kamalnath"],
-  },
-  {
-    id: 2,
-    name: "Aayushi",
-    friends: ["Bhaskar"],
-  },
-  {
-    id: 3,
-    name: "Bhaskar",
-    friends: [],
-  },
-  {
-    id: 4,
-    name: "Kamalnath",
-    friends: ["Shanti"],
-  },
-  {
-    id: 5,
-    name: "Shanti",
-    friends: ["Bhaskar"],
-  },
+    {
+        id: 1,
+        name: "Sameer",
+        friends: ["Aayushi", "Kamalnath"],
+    },
+    {
+        id: 2,
+        name: "Aayushi",
+        friends: ["Bhaskar"],
+    },
+    {
+        id: 3,
+        name: "Bhaskar",
+        friends: [],
+    },
+    {
+        id: 4,
+        name: "Kamalnath",
+        friends: ["Shanti"],
+    },
+    {
+        id: 5,
+        name: "Shanti",
+        friends: ["Bhaskar"],
+    },
 ];
 
 function App() {
-  const [users, setUsers] = useState(usersData);
+    const [users, setUsers] = useState(usersData);
 
-  const addUserHandler = (person) => {
-    const newUser = [...users, { ...person, id: users.length + 1 }];
-    setUsers(newUser);
-  };
-  const addFriendHandler = (userInfo, userFriends) => {
-    let newUsers = users.reduce((acc, item) => {
-      if (item.id === userInfo.id) {
-        return [...acc, { id: item.id, name: item.name, friends: userFriends }];
-      } else return [...acc, item];
-    }, []);
+    const addUserHandler = person => {
+        const newUser = [...users, { ...person, id: users.length + 1 }];
+        setUsers(newUser);
+    };
+    const addFriendHandler = (userInfo, userFriends) => {
+        let newUsers = users.reduce((acc, item) => {
+            if (item.id === userInfo.id) {
+                return [...acc, { id: item.id, name: item.name, friends: userFriends }];
+            } else return [...acc, item];
+        }, []);
 
-    setUsers(newUsers);
-  };
+        setUsers(newUsers);
+    };
 
-  return (
-    <div className="App">
-      <div className="addpeople-section">
-        <div className="inner-section">
-          <AddUser addUserfn={addUserHandler} />
-          {users.length > 2 ? (
-            <AddFriends allusers={users} addFriendFn={addFriendHandler} />
-          ) : (
-            <p>Please add two friends, atleast.</p>
-          )}
+    return (
+        <div className="App">
+            <div className="addpeople-section">
+                <div className="inner-section">
+                    <AddUser addUserfn={addUserHandler} />
+                    {users.length > 2 ? (
+                        <AddFriends allusers={users} addFriendFn={addFriendHandler} />
+                    ) : (
+                        <p>Please add two friends, atleast.</p>
+                    )}
+                </div>
+            </div>
+            <div className="relationship-section">
+                <MakeRelationship allusers={users} />
+            </div>
         </div>
-      </div>
-      <div className="relationship-section">
-        <MakeRelationship allusers={users} />
-      </div>
-    </div>
-  );
+    );
 }
 
 export default App;
